0972496b6777543baec4f44eab19bc7cc3749acb
Marlon Blake <greg@gregmalcolm.com>
2018-03-03 13:07:42 -0500

    Added some libs, and a placeholder for systems

---
+++ Barnacle/Models/Systems.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barnacle.Models
+{
+    public class Systems
+    {
+        public List<String> all
+        {
+            get
+            {
+                return new List<String>() {
+                    "Eleu",
+                    "Santjalan",
+                    "Noatine",
+                    "Kpaniya",
+                    "Suessetani",
+                    "Alowis",
+                    "Upannoda",
+                    "BD-11 4280",
+                    "20 Ophiuchi",
+                    "Watha",
+                    "Mechet",
+                    "HR 6189",
+                    "Paledawa",
+                    "Andaris",
+                    "HIP 84696",
+                    "Mahamanpa",
+                    "Purungarang"
+                };
+            }
+        }
+    }
+}
