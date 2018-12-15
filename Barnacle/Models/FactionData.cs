ac482ccf6fccbe8be53e2b5d449efe32a31a1ceb
Marlon Blake <greg@gregmalcolm.com>
2018-12-15 15:04:29 -0500

    This is where I left off apparently

--- Barnacle/Models/FactionData.cs
+++ Barnacle/Models/FactionData.cs
@@ -8,5 +8,21 @@ namespace Barnacle.Models
 {
     public class FactionData
     {
+        public String Source { get; set; }
+        public String System { get; set; }
+        public String Faction { get; set; }
+
+        public DateTime EntryDate { get; set; }
+        public String Commander { get; set; }
+
+        public Decimal Influence { get; set; }
+        public String State { get; set; }
+
+        public String Government { get; set; }
+        public String Allegiance { get; set; }
+
+        public List<StateDetail> RecoveringStates { get; set; }
+        public List<StateDetail> PendingStates { get; set; }
+        //Public Property Found As Boolean = False    
     }
 }
---
+++ Barnacle/Models/StateDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barnacle.Models
+{
+    public class StateDetail
+    {
+        public int Trend { get; set; }
+        public String State { get; set; }
+    }
+}
